d9737d45223b39ac649fb08f0546848ffbbb2d51
malko <jgotti@jgotti.org>
2013-08-04 04:58:16 +0200

    finally starting to write unit test for D specific features

--- gruntfile.js
+++ gruntfile.js
@@ -1,23 +1,34 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/* (c) Jonathan Gotti - licence: https://github.com/malko/d.js/LICENCE.txt */\n'
-      },
-      build: {
-        src: 'lib/D.js',
-        dest: 'lib/D.min.js'
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/* (c) Jonathan Gotti - licence: https://github.com/malko/d.js/LICENCE.txt */\n'
+			},
+			build: {
+				src: 'lib/D.js',
+				dest: 'lib/D.min.js'
+			}
+		},
+		mochacli: {
+			options: {
+				require: ['chai'],
+				reporter: 'spec',
+				timeout: 300
+			},
+			src: { options: {files: 'tests/d-src*.js'}},
+			min: { options: {files: 'tests/d-min*.js'}}
+		}
+	});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-mocha-cli');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+	// Default task(s).
+	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('test', ['mochacli']);
 
 };
